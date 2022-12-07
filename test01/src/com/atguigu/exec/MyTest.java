c2d72054af7c784756b383bc5542640a432537ea
Kevin9780 <shexianzx@foxmail.com>
2022-12-07 16:58:16 +0800

    本地修改2

--- test01/src/com/atguigu/exec/MyTest.java
+++ test01/src/com/atguigu/exec/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("本地修改");
+        System.out.println("本地修改2");
     }
 }
