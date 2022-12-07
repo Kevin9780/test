66f69fbc17633b1b1b0256345414e48c9b2b7d49
Kevin9780 <shexianzx@foxmail.com>
2022-12-07 16:41:02 +0800

    本地修改

--- test01/src/com/atguigu/exec/MyTest.java
+++ test01/src/com/atguigu/exec/MyTest.java
@@ -3,5 +3,6 @@ package com.atguigu.exec;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("本地修改");
     }
 }
